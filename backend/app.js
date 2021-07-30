cd159da7a217eac6998032098dadb1916f732541
parag56 <paragthakur56@gmail.com>
2021-07-31 00:20:07 +0530

    firstcommit

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(isAuth);
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(
+    `$process.env.DATABASE_URL`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
